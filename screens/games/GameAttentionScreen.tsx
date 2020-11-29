747fc10f1588b0fede32b70798dad18d27837d13
Henrique Macedo <mail@henriquemacedo.pt>
2020-11-29 11:16:02 +0000

    Detect touches and get data
    - onPressIn
    - onPressOut
    - target
    - pageX
    - pageY
    - locationX
    - locationY

--- screens/games/GameAttentionScreen.tsx
+++ screens/games/GameAttentionScreen.tsx
@@ -1,22 +1,70 @@
-import * as React from "react";
+import React, { useState } from "react";
 import styled from "styled-components/native";
+import { Text, Pressable, TouchableWithoutFeedback } from "react-native";
 
-const StyledView = styled.View`
-  background-color: white;
+const Wrapper = styled.View`
   flex: 1;
+  flex-direction: row;
   align-items: center;
   justify-content: center;
+  flex-wrap: wrap;
 `;
 
-const StyledText = styled.Text`
-  font-size: 20px;
+const Card = styled.View`
+  border: 1px solid red;
+  height: 100%;
+  box-shadow: 0 0 5px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+`;
+
+const CardTitle = styled.Text`
+  font-size: 40px;
   font-weight: bold;
+  color: #000;
 `;
 
 export default function GameAttentionScreen() {
+  const [timesPressed, setTimesPressed] = useState(0);
+  const safeAreaTouch = () => console.log("touch safe area");
+
+  let textLog = "";
+  if (timesPressed > 1) {
+    textLog = timesPressed + "x onPress";
+  } else if (timesPressed > 0) {
+    textLog = "onPress";
+  }
+
+  console.log(textLog);
+
   return (
-    <StyledView>
-      <StyledText>Game Attention</StyledText>
-    </StyledView>
+    <Wrapper>
+      <TouchableWithoutFeedback onPress={safeAreaTouch}>
+        <Card
+          onTouchStart={(e) => {
+            console.log("touchMove", e.nativeEvent);
+          }}
+          onTouchEnd={(e) => {
+            console.log("touchEnd", e.nativeEvent);
+          }}
+        >
+          <CardTitle>Attention</CardTitle>
+          <Pressable
+            onPressIn={() => {
+              setTimesPressed((current) => current + 1);
+            }}
+            onPressOut={() => {
+              setTimesPressed((current) => current - 1);
+            }}
+            style={({ pressed }) => [
+              {
+                backgroundColor: pressed ? "rgb(210, 230, 255)" : "white",
+              },
+            ]}
+          >
+            {({ pressed }) => <Text>{pressed ? "Pressed!" : "Press Me"}</Text>}
+          </Pressable>
+        </Card>
+      </TouchableWithoutFeedback>
+    </Wrapper>
   );
 }
